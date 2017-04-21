f9b831550838bdb8a4269dc384e80e395b4e7026
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-21 20:02:32 -0300

    Add files via upload

---
+++ Hola_Mundo.java
@@ -0,0 +1,28 @@
+
+package ejerciciosjava;
+
+/**
+ *
+ * @author fer
+ */
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola Mundo");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
