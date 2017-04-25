80829dd6fac710bfb632739a4d907ca275029f28
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-25 02:27:51 -0300

    Ejercicio 2

---
+++ JavaEjerc2.java
@@ -0,0 +1,23 @@
+
+package javaejerc2;
+
+
+public class JavaEjerc2 {
+
+    public static void main(String[] args) {
+        
+        int alumnos = 36;
+        int bancos = 30;
+        int n_bancos = alumnos -bancos ;
+        
+        if(n_bancos <= 0 );{
+        
+            /*System.out.println("Sobran : " + n_bancos " Bancos");
+        
+        }else; 
+        {*/
+            System.out.println("Faltan " + n_bancos+ " bancos");
+        }
+    }   
+    
+}
