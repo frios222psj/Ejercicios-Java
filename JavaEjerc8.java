5389d84b0ae87b4060a0fa49965c203fb3637136
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-29 18:48:36 -0300

    JavaEjerc_8

---
+++ JavaEjerc8.java
@@ -0,0 +1,15 @@
+package javaejerc8;
+
+public class JavaEjerc8 {
+
+        public static void main(String[] args) {
+        
+            int c = 0;
+            while (c <= 50){
+                /*=1;*/
+                System.out.println (c);  
+                c+=1;
+            }
+    }
+}
+
