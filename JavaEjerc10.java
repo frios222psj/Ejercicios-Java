b01c51d2c320be5241300a90ae6cbd49dc44f84a
Fernando Luis Rios <frios222psj@gmail.com>
2017-04-29 01:03:23 -0300

    JavaEjerc10.java

---
+++ JavaEjerc10.java
@@ -0,0 +1,19 @@
+package javaejerc10;
+
+public class JavaEjerc10 {
+
+    public static void main(String[] args) {
+        
+        int cont = -2;
+        int num = -2;
+        
+        while(cont<40){
+            num += 1;
+            if(num %2 == 0){
+                System.out.println(num);
+            }
+            cont += 1;
+        }
+    }
+}
+   
